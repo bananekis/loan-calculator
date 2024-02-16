23b2e034127cf98208cb3a1175214c144963c51b
Lukas Novak <nlukas187@gmail.com>
2024-02-16 20:40:08 +0100

    env

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 import { LoanOptions } from './types';
 
-export const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+export const baseUrl = process.env.NEXT_PUBLIC_API_URL || undefined;
 
 export const apiUrl = '/api';
 
